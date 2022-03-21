52f8e6c8e7c9c72724a32fca07e0ef2f86611c2d
Maksym <maksy.z@integritassolutions.net>
2022-03-21 04:19:42 +0200

    refactoring setting

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,15 @@ FROM node:16-alpine
 
 ENV PORT=8880
 ENV HOST=localhost
-# need to add AWS keys
-ENV AWS_BUCKET=gm-docs-bucket
+# ENV AWS_KEY=PASTE HERE YOUR KEY
+# ENV AWS_SECRET=PASTE HERE YOUR KEY
+# ENV AWS_BUCKET=PASTE HERE YOUR BACKET
 ENV AWS_LOCATION=eu-west-3
-ENV CLIENT_SERVICE_URL=http://client:3000/graphql
+ENV CLIENT_SERVICE_URL=http://client:8080/graphql
 
 WORKDIR /app
 COPY package*.json ./
 RUN npm install --only=production
 COPY --from=0 /app/dist ./dist
 EXPOSE 8880
-CMD [ "npm", "start"]
+CMD [ "npm", "run", "start:prod"]
--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,16 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { Args, Int, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { AppService } from './app.service';
 
 @Resolver()
 export class AppResolver {
+  private log = new Logger(AppResolver.name);
+
   constructor(private readonly appService: AppService) {}
 
   @Mutation(() => String, { name: 'uploadFile', nullable: true })
   async uploadFile(
-    @Args('file', { type: () => [GraphQLUpload] }) file: FileUpload[],
+    @Args('files', { type: () => [GraphQLUpload] }) files: FileUpload[],
   ): Promise<void> {
-    this.appService.uploadFiles(file);
+    this.log.log(files);
+    this.appService.uploadFiles(files);
   }
 
   @Query(() => String)
